39136cae0809dd9d7be07490cbfd9f7449d8be7c
Andrew Che <andrew@codeninja55.me>
2018-08-05 19:33:18 +1000

    ADded material-ui/core

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
+// CLASS-BASED COMPONENTS
+export default class App extends Component {
   render() {
+    // JSX Return format
     return (
       <div className="App">
         <h1>Hello, World!</h1>
+        <Person />
       </div>
     );
+
+    // React build original format
+    // return React.createElement('h1', null, 'Does this work now?');
+
+    // React build original format with nesting
+    /*return React.createElement('div', {className: 'App'},
+        React.createElement('h1', null, 'Does this work yet?')
+    );*/
   }
 }
-
-export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,13 @@
+// FUNCTIONAL COMPONENTS
+// Another way of making a component, still with ES6 convention and JSX
+import React from 'react';
+
+const person = () => {
+  return (
+      <div>
+        <p>I'm a Person</p>
+      </div>
+  );
+};
+
+export default person;
